0e6f8826bc722c7c211549b59d2530d893d1488c
Мороз Артем <moroz.artem3@wildberries.work>
2025-04-13 23:58:32 +0300

    save

--- main.cpp
+++ main.cpp
@@ -6,6 +6,22 @@ int main() {
     int width = 256;
     int height = 256;
     
-    cout << "width: " << width << " " << "height: " << height << endl;
+    cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    for (int i = 0; i < height; i++) {
+        clog << "\rScanlines remaining: " << height - i << ' ' << flush;
+        for (int j = 0; j < width; j++) {
+            auto r = double(i) / (width-1);
+            auto g = double(j) / (height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    clog << "\rDone.\n";
 }
 
