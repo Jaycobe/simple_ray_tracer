97095c20b708b2cb666426c2ded37b9d3ac656df
Мороз Артем <moroz.artem3@wildberries.work>
2025-04-13 23:32:11 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int width = 256;
+    int height = 256;
+    
+    cout << "width: " << width << " " << "height: " << height << endl;
+}
+
