f57bbfb0da981c78cc6f8afa82503eb5722cab56
moroz.artem3 <moroz.artem3@wildberries.work>
2026-09-02 19:40:31 +0300

    add mvp frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,76 @@
 import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {RenderScene} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Hello!");
 
     type RenderMode = "raytrace" | "lightweight";
-    const [mode, setMode] = useState<RenderMode>("raytrace");  // annotate
+    const [mode, setMode] = useState<RenderMode>("raytrace");
     const [busy, setBusy] = useState(false);
+    const [imageSrc, setImageSrc] = useState<string | null>(null)
+    const [error, setErrorMessage] = useState<string | null>(null)
+    const [elapsedTime, setElapsedTime] = useState<number | null>(null)
+
 
     return (
         <div id="App">
             <div id="text" className="result">{mode}</div>
-            <label>
-                <input
-                    type="radio"
-                    name="mode"
-                    checked={mode === 'lightweight'}
-                    onChange={() => setMode("lightweight")}
-                />
-                Lightweight
-            </label>
+            <div id="radiobox">
+                <label>
+                    <input
+                        type="radio"
+                        name="mode"
+                        checked={mode === 'lightweight'}
+                        onChange={() => setMode("lightweight")}
+                    />
+                    Lightweight
+                </label>
 
-            <label>
-                <input
-                    type="radio"
-                    name="mode"
-                    checked={mode === 'raytrace'}
-                    onChange={() => setMode("raytrace")}
-                />
-                RayTrace
-            </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="mode"
+                        checked={mode === 'raytrace'}
+                        onChange={() => setMode("raytrace")}
+                    />
+                    RayTrace
+                </label>
+            </div>
+
+            <div id="input" className="input-box">
+                <button className="btn" disabled={busy}
+                        onClick={async () => {
+                            setBusy(true);
+                            setErrorMessage(null)
+                            try {
+                                const start = performance.now()
+                                const b64 = await RenderScene(mode, 800, 600);
+                                const end = performance.now()
+                                setElapsedTime(end - start)
+                                setImageSrc(`data:image/png;base64,${b64}`)
+                            } catch (error) {
+                                setErrorMessage(String(error))
+                                setImageSrc(null)
+                            } finally {
+                                setBusy(false);
+                            }
+                        }}
+                >
+                    Render
+                </button>
+            </div>
+
+            <div id="imgBox">
+                {imageSrc && <img alt="Rendered Image" src={imageSrc}/>}
+            </div>
+
+            <div id="errBox">
+                {error && <div>{error}</div>}
+            </div>
+
+            <div id="perfomanceBox">
+                {elapsedTime && <div>{elapsedTime}</div>}
+            </div>
         </div>
     )
 }
